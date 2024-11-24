d167ed2db91841f4d3cfa5211264b91deaabc17b
Greque <ichijou_raku@hotmail.com>
2024-11-24 13:29:03 -0300

    uwu

---
+++ assets/js/a1_Bootstrap.js
@@ -0,0 +1,10 @@
+document.getElementById("id1TextColumnsHTML").textContent =
+`<div class="row">
+    <div class="col-2"><div>a</div></div>
+    <div class="col-4"><div>a</div></div>
+    <div class="col-6"><div>a</div></div>
+    <div class="col"><div></div></div>
+</div>`;
+
+document.getElementById("id1TextColumnsCSS").textContent =
+`.section2 .row { margin: 0; }`;
